58462813d432b01d2cf758e33f3dc0f468a5ad03
David Waltereit <david.waltereit@zetvisions.de>
2015-12-08 09:32:16 +0000

    First version

--- controller/Master.controller.js
+++ controller/Master.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("zv.template.masterdetail.controller.Master", {
+	return Controller.extend("zv.application.profile.controller.Master", {
 		onPressListItem: function(oEvent){
 			var oItem = oEvent.getSource();
 			console.log(oItem);
